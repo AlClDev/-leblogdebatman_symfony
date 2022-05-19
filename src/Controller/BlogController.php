4790e54c687e3841e61fca31bcf49903a766ff5a
AlCl <a@a.a>
2022-05-19 14:54:31 +0200

    Ajout du système de pagination

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Form\NewArticleFormType;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
@@ -78,15 +80,27 @@ class BlogController extends AbstractController
 
 
     /**
-     * Contrôleur de la page ui listes les articles
+     * Contrôleur de la page qui listes les articles
      */
     #[Route('/publication/liste/', name: 'publication_list')]
-    public function publicationList(ManagerRegistry $doctrine): Response
+    public function publicationList(ManagerRegistry $doctrine, Request $request, PaginatorInterface $paginator): Response
     {
+        // Récupération de $_GET['page'], 1 si elle n'existe pas
+        $requestedPage = $request->query->getInt('page', 1);
 
-        $articleRepo = $doctrine->getRepository(Article::class);
+        if($requestedPage < 1){
+            throw new NotFoundHttpException();
+        }
 
-        $articles = $articleRepo->findAll();
+        $em=$doctrine->getManager();
+
+        $query=$em->createQuery('SELECT a FROM App\Entity\Article a ORDER BY a.publicationDate DESC');
+
+        $articles = $paginator->paginate(
+            $query,     //Requête créée juste avant
+            $requestedPage,     //Page qu'on souhaite voir
+            10,     //Nombre d'article à afficher par page
+        );
 
         return $this->render('blog/publication_list.html.twig', [
             'articles' => $articles,
